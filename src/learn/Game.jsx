fa5f8110332bca4869d576c0b5695aba025ae01a
Richey24 <uahomorejoice@gmail.com>
2022-12-28 15:40:48 +0100

    changes

--- src/learn/Game.jsx
+++ src/learn/Game.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import "./Game.css"
+import cancel from "../img/cancel.svg"
 
 let timer;
 const Game = () => {
@@ -38,24 +39,36 @@ const Game = () => {
         setClick(true)
         document.getElementById("learnMore").style.visibility = "visible"
     }
+
+    const showModal = () => {
+        document.getElementById("learnModal").classList.toggle("showLearn")
+    }
+
     return (
-        <div className="mainGame">
-            <div>
-                <span id="quizTimer">60</span>
-                <h1>Question 1</h1>
-                <p>Who was the first son of Jacob?</p>
-                <div className="theAnswer">
-                    <p>A. <span onClick={(e) => checkAnswer(e, false)}>Joseph</span></p>
-                    <p>B. <span onClick={(e) => checkAnswer(e, true)}>Reuben</span></p>
-                    <p>C. <span onClick={(e) => checkAnswer(e, false)}>Dan</span></p>
-                    <p>D. <span onClick={(e) => checkAnswer(e, false)}>Asher</span></p>
-                </div>
-                <div className="learnButton">
-                    <button id="learnMore" className="learnMore">Learn more</button>
-                    <button className="nextQuest">Next question</button>
+        <div>
+            <div className="mainGame">
+                <div>
+                    <span id="quizTimer">60</span>
+                    <h1>Question 1</h1>
+                    <p>Who was the first son of Jacob?</p>
+                    <div className="theAnswer">
+                        <p>A. <span onClick={(e) => checkAnswer(e, false)}>Joseph</span></p>
+                        <p>B. <span onClick={(e) => checkAnswer(e, true)}>Reuben</span></p>
+                        <p>C. <span onClick={(e) => checkAnswer(e, false)}>Dan</span></p>
+                        <p>D. <span onClick={(e) => checkAnswer(e, false)}>Asher</span></p>
+                    </div>
+                    <div className="learnButton">
+                        <button onClick={() => showModal()} id="learnMore" className="learnMore">Learn more</button>
+                        <button className="nextQuest">Next question</button>
+                    </div>
                 </div>
             </div>
+            <div id="learnModal" className="learnModal">
+                <img onClick={showModal} src={cancel} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nisi rem omnis aliquam, culpa sapiente magni error officiis architecto quos atque, deserunt ullam laborum maxime, nam expedita. Quae, sequi accusamus? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus dolore quasi dignissimos fugiat praesentium, atque vitae deserunt maiores. Tenetur illum quod, aliquam enim quisquam explicabo quo perferendis ad sit id! Lorem ipsum dolor sit amet consectetur adipisicing elit. A blanditiis deleniti, esse architecto tempore nobis, ipsum nemo, cupiditate consequuntur voluptatem eveniet maxime libero quaerat error aperiam fuga sequi rem laudantium? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse saepe a tempore, facere aperiam impedit. Laboriosam fuga ullam totam, delectus iure, sequi quo repellat ex quas consectetur nihil perspiciatis sapiente.</p>
+            </div>
         </div>
+
     )
 }
 
--- src/learn/Names.jsx
+++ src/learn/Names.jsx
@@ -2,12 +2,15 @@ import { useEffect } from "react"
 import { useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import "./Names.css"
+import drop from "../img/drop.svg"
 
 const Names = () => {
     const [arr, setArr] = useState([1])
+    const [tribe, setTribe] = useState(["Asher"])
     const { state } = useLocation()
     const navigate = useNavigate()
     const count = state?.count
+
     useEffect(() => {
         if (!count) {
             navigate("/bible/select")
@@ -25,13 +28,43 @@ const Names = () => {
         })
         navigate("/bible/game")
     }
+
+    const selectTribe = (value) => {
+        document.getElementById(value).classList.toggle("show")
+    }
+
+    const getTribe = (value) => {
+        setTribe(value)
+        selectTribe()
+    }
+
     return (
         <div className="mainNames">
             <form onSubmit={submitForm}>
                 <h4>Enter player(s) name</h4>
                 {
                     arr.map((ar, i) => (
-                        <input required className="playerName" key={i} placeholder={`Enter player ${ar} name`} type="text" />
+                        <div key={i} >
+                            <input required className="playerName" placeholder={`Enter player ${ar} name`} type="text" />
+                            <div className='tribeDiv'>
+                                <label>Select player {ar} tribe</label>
+                                <p onClick={() => selectTribe(`tribe${i}`)} className='tribeMain'>{tribe[0]} <img src={drop} alt="" /></p>
+                                <ul id={`tribe${i}`} className='tribeList'>
+                                    <li onClick={() => getTribe(["Asher"])}>Asher</li>
+                                    <li onClick={() => getTribe(["Dan"])}>Dan</li>
+                                    <li onClick={() => getTribe(["Ephraim"])}>Ephraim</li>
+                                    <li onClick={() => getTribe(["Gad"])}>Gad</li>
+                                    <li onClick={() => getTribe(["Issachar"])}>Issachar</li>
+                                    <li onClick={() => getTribe(["Manasseh"])}>Manasseh</li>
+                                    <li onClick={() => getTribe(["Naphtali"])}>Naphtali</li>
+                                    <li onClick={() => getTribe(["Reuben"])}>Reuben</li>
+                                    <li onClick={() => getTribe(["Simeon"])}>Simeon</li>
+                                    <li onClick={() => getTribe(["Zebulun"])}>Zebulun</li>
+                                    <li onClick={() => getTribe(["Judah"])}>Judah</li>
+                                    <li onClick={() => getTribe(["Benjamin"])}>Benjamin</li>
+                                </ul>
+                            </div>
+                        </div>
                     ))
                 }
                 <button type="submit">Submit</button>
