c99c243f20d85e55ec04bd71ecd13ca292549a2b
Lionel Francis <lionelishmael@gmail.com>
2023-01-31 22:56:14 -0500

    fix merge conflict

--- src/views/Crossword/components/CrosswordPuzzle.jsx
+++ src/views/Crossword/components/CrosswordPuzzle.jsx
@@ -489,7 +489,7 @@ export default function CrosswordPuzzle(props) {
      }
  
 
-   return <div className="min-h-screen bg-cover bg-no-repeat bg-fixed bg-center" style={{ backgroundImage: `url('bg/${props.background}')` }}>
+   return <div className="min-h-screen bg-cover bg-no-repeat bg-fixed bg-center" style={{ backgroundImage: `url('https://absa7kzimnaf.blob.core.windows.net/newcontainer/${props.background}')` }}>
               <button onClick={handleShow }>test</button>
            <div className="grid h-screen place-items-center overflow-auto" >
                 <div>
