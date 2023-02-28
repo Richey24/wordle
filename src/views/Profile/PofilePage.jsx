87880a69f7bd6334949ccac35c6aa339edd68681
javon.ellis <AlwaysUse@Fork21234>
2023-02-27 21:43:22 -0500

    Update Customer Portal - Disable link to display billing if not qualified for it

--- src/views/Profile/PofilePage.jsx
+++ src/views/Profile/PofilePage.jsx
@@ -92,6 +92,36 @@ export default function ProfilePage() {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="hidden sm:block" aria-hidden="true">
+                            <div className="py-5">
+                                <div className="border-t border-gray-200" />
+                            </div>
+                        </div>
+
+                        {user.paid ?
+                        <div className="mt-10 sm:mt-0">
+                            <div className="md:grid md:grid-cols-3 md:gap-6">
+                                <div className="md:col-span-1">
+                                    <div className="px-4 sm:px-0">
+                                        <h3 className="text-base font-semibold leading-6 text-gray-900">Subscription</h3>
+                                        <p className="mt-1 text-sm text-gray-600">Update your card or change your plan.</p>
+                                    </div>
+                                </div>
+                                <div className="md:col-span-2 md:mt-0">
+                                    <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                                        <button
+                                            type="submit"
+                                            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                            onClick={() =>window.open('https://billing.stripe.com/p/login/bIY3gifgJ8Hb0ZW288', '_blank')}
+
+                                        >
+                                            Update Subscription Details
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> : "" }
                     </div>
                 </div>
             </div>
