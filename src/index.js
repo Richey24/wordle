4097fcae14e6a7eef3555eda2e31556962642574
Rejoice <uahomorejoice@gmail.com>
2023-03-16 01:25:50 +0100

    Revert "minor fix"
    This reverts commit 68b9714b085d529397489cb97ea43c135b61d963.

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ import { ThemeProvider } from "@material-tailwind/react";
 import { MaterialTailwindControllerProvider } from "./context"
 import CancelSub from './views/Profile/CancelSub';
 import Hebrew from './hebrew/Hebrew';
-import AddHebrew from './hebrew/AddHebrew';
+// import AddHebrew from './hebrew/AddHebrew';
 
 import { UserProfile } from './pages/account/user-profile';
 import Deck from './hebrew/Deck';
@@ -109,9 +109,8 @@ root.render(
             <Route path="trash" element={<ToBeDel />} />
             <Route path="leaderboard" element={<AdminLeaderboard />} />
             <Route path="activities" element={<Activity />} />
-            <Route path="deck" element={<Deck />} />
-            <Route path="hebrew/:deck" element={<Hebrew />} />
-            <Route path="add/hebrew/:deck" element={<AddHebrew />} />
+            <Route path="hebrew" element={<Hebrew />} />
+            {/* <Route path="add/hebrew" element={<AddHebrew />} /> */}
           </Route>
           {/* <Route path="/user-account" element={<AccountSettings />}>
               <Route path="" element={<UserProfile />} />
