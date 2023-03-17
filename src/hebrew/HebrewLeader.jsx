7dc4bf53a2a585dfcd7901d4ccdda284f37586dd
Rejoice <uahomorejoice@gmail.com>
2023-03-17 03:17:04 +0100

    hebrew game almost done

---
+++ src/hebrew/HebrewLeader.jsx
@@ -0,0 +1,205 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
+import { Spinner } from "react-bootstrap"
+import url from "../url"
+import "../views/Leader/leader.css"
+import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image'
+import Card from 'react-bootstrap/Card';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faToolbox, faPlus, faHome } from '@fortawesome/fontawesome-free-solid'
+
+import Modal from 'react-bootstrap/Modal';
+
+import benjamin from "../img/benjamin.png"
+import dan from "../img/dan.png"
+import ephraim from "../img/ephraim.png"
+import gad from "../img/gad.png"
+import issachar from "../img/issachar.png"
+import joseph from "../img/joseph.png"
+import judah from "../img/judah.png"
+import levi from "../img/levi.png"
+import manasseh from "../img/manasseh.png"
+import naftali from "../img/naftali.png"
+import reuben from "../img/reuben.png"
+import simeon from "../img/simeon.png"
+import zebulun from "../img/zebulun.png"
+import asher from "../img/asher.png"
+import { useNavigate } from "react-router-dom"
+import video from "../utils/video"
+import THeHeader from "../components/TheHeader"
+
+
+const images = {
+    benjamin: benjamin,
+    dan: dan,
+    ephraim: ephraim,
+    gad: gad,
+    issachar: issachar,
+    joseph: joseph,
+    judah: judah,
+    levi: levi,
+    manasseh: manasseh,
+    naftali: naftali,
+    reuben: reuben,
+    simeon: simeon,
+    zebulun: zebulun,
+    asher: asher
+}
+
+const HebrewLeader = () => {
+    const [users, setUsers] = useState([])
+    const [spin, setSpin] = useState(false)
+    const token = localStorage.getItem("token")
+    const navigate = useNavigate()
+
+    const [bground, setBackground] = useState(() => {
+        // getting stored value
+        const saved = localStorage.getItem("background1");
+        const initialValue = saved;
+        return initialValue || "";
+    });
+
+    // Pop functionalies goes here
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = (e) => {
+        e.preventDefault();
+        setShow(true);
+    }
+
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get(`${url}/user/find/all`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                validateStatus: () => true
+            })
+            const rep = await res.data
+            console.log(rep);
+            if (res.status !== 200) {
+                setSpin(false)
+                navigate("/login")
+                return
+            }
+            const arr = rep.sort((a, b) => b.hebrewScore - a.hebrewScore)
+            setUsers(arr)
+            setSpin(false)
+        })()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    if (spin) {
+        return (
+            <div style={{
+                width: "100%",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Spinner animation="border" color="#3d1152" />
+            </div>
+        )
+    }
+
+    return (
+        <Container fluid id="videowrapper" className="bg-purple  min-vh-100 video-container">
+            <THeHeader />
+            <video src={bground} autoPlay muted loop id='thevid' />
+            <Container fluid id="videoMessage">
+                <Container>
+
+                    <Row>
+                        <Col className="text-center mt-5 mb-5">
+                            <h3 className="text-white">Hebrew Word Weekly Leader Board</h3>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col className="text-center">
+                            <Table borderless className="justify-content-center">
+                                <thead className="border-bottom">
+                                    <tr className="text-white">
+                                        <th>Username</th>
+                                        <th>Tribe</th>
+                                        <th>School</th>
+                                        <th>Country</th>
+                                        <th>Score</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="text-white align-middle">
+                                    {
+                                        users.map((user, i) => (
+                                            user?.hebrewScore !== 0 &&
+                                            <tr key={i}>
+                                                <td style={{ textTransform: "uppercase" }}>{user?.username}</td>
+                                                <td style={{ display: "flex", justifyContent: "center" }}><Image fluid src={images[user?.tribe[0]?.toLowerCase()]} alt="" style={{ width: 160, height: 160 }} /></td>
+                                                <td>{user?.church}</td>
+                                                <td style={{ width: "160px", height: "160px" }}><img src={user?.country[1]} alt="" /></td>
+                                                <td>{user?.hebrewScore}</td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+
+
+
+            <Modal size="lg" show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Change Video Background </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        {
+                            video.map((vid, i) => (
+                                <Col key={i} md={3}>
+                                    <Card >
+                                        <video onClick={() => setBackground(`https://absa7kzimnaf.blob.core.windows.net/newcontainer/${vid}.mp4`)} className="card-video" autoPlay muted loop>
+                                            <source src={`https://absa7kzimnaf.blob.core.windows.net/newcontainer/${vid}.mp4`} type="" />
+                                        </video>
+                                    </Card>
+                                </Col>
+                            ))
+                        }
+                    </Row>
+                    <button onClick={() => setShow(false)} style={{ marginLeft: "85%" }} className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Close</button>
+                </Modal.Body>
+            </Modal>
+
+            <div className="fab-container">
+                <div className="fab shadow">
+                    <div className="fab-content">
+                        <FontAwesomeIcon icon={faPlus} className="text-white" />
+                    </div>
+                </div>
+                <div className="sub-button shadow">
+                    <a onClick={handleShow} href="/" target="_blank">
+                        <FontAwesomeIcon icon={faToolbox} className="text-white" />
+                    </a>
+                </div>
+
+                <div className="sub-button shadow">
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faHome} className="text-white" />
+                    </Link>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+export default HebrewLeader
--- src/hebrew/MainHebrew.jsx
+++ src/hebrew/MainHebrew.jsx
@@ -5,6 +5,9 @@ import axios from "axios";
 import url from "../url";
 import CongratulationView from "../components/Congratulation/CongratulationView";
 import FailedDiv from "../components/Congratulation/Failed";
+import THeHeader from "../components/TheHeader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faUserGraduate } from "@fortawesome/fontawesome-free-solid";
 
 let interval = null
 const MainHebrew = () => {
@@ -15,6 +18,8 @@ const MainHebrew = () => {
     const [won, setWon] = useState(false)
     const [failed, setFailed] = useState(false)
     const [images, setImages] = useState([])
+    const [score, setScore] = useState(0)
+    const [count, setCount] = useState(0)
     const navigate = useNavigate()
 
     const getItems = async () => {
@@ -44,11 +49,13 @@ const MainHebrew = () => {
 
     useEffect(() => {
         getItems()
+        window.scrollTo(0, document.body.scrollHeight)
         const timer = document.getElementById("hebTime")
         interval = setInterval(() => {
-            if (Number(timer.innerHTML <= 0)) {
+            if (Number(timer.innerHTML) <= 0) {
                 const card = document.querySelector(".card__inner");
                 card.classList.toggle('is-flipped');
+                setFailed(true)
                 clearInterval(interval)
                 return
             }
@@ -57,14 +64,15 @@ const MainHebrew = () => {
         return () => {
             clearInterval(interval)
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const flipCard = (val) => {
         if (failed || won) {
             return
         }
-        const score = document.getElementById("hebrewScore")
         const card = document.querySelector(".card__inner");
+        const timer = document.getElementById("hebTime")
         card.classList.toggle('is-flipped');
         if (val === lists[num].correctImage) {
             const sound = new Audio(require("../sound/Celebration.mp3"))
@@ -72,8 +80,9 @@ const MainHebrew = () => {
             setTimeout(() => {
                 sound.pause()
             }, 3000)
+            setCount(count + 1)
             setWon(true)
-            score.innerHTML = Number(score.innerHTML) + 100
+            setScore(score + Number(timer.innerHTML))
         } else {
             setFailed(true)
             new Audio(require("../sound/wrong.mp3")).play()
@@ -86,11 +95,21 @@ const MainHebrew = () => {
         card.classList.toggle('is-flipped');
         const timer = document.getElementById("hebTime")
         timer.innerHTML = 60
+        interval = setInterval(() => {
+            if (Number(timer.innerHTML <= 0)) {
+                const card = document.querySelector(".card__inner");
+                card.classList.toggle('is-flipped');
+                setFailed(true)
+                clearInterval(interval)
+                return
+            }
+            timer.innerHTML = Number(timer.innerHTML) - 1
+        }, 1000);
         let imgArr = [];
         while (imgArr.length < 3) {
             let rand = Math.floor(Math.random() * lists.length)
             let randImg = lists[rand].correctImage
-            if (!imgArr.includes(randImg) && randImg !== lists[0].correctImage) {
+            if (!imgArr.includes(randImg) && randImg !== lists[num + 1].correctImage) {
                 imgArr.push(randImg)
             }
         }
@@ -105,12 +124,14 @@ const MainHebrew = () => {
     }
 
     return (
-        <div>
+        <div style={{ paddingBottom: "20px" }}>
+            <THeHeader />
             <div class="hebrewMainDiv">
                 <div class="hebrewFirst">
                     <div className="hebrewCard" onClick={() => flipCard(images[0])} ><img src={`https://absa7kzimnaf.blob.core.windows.net/newcontainer/${images[0]}`} alt="" /></div>
                     <div style={{ backgroundImage: state }} className="hebrewTimer">
-                        <p id="hebTime">60</p>s
+                        <span>Timer</span>
+                        <div style={{ display: "flex" }}><p id="hebTime">60</p>s</div>
                     </div>
                     <div className="hebrewCard" onClick={() => flipCard(images[1])} ><img src={`https://absa7kzimnaf.blob.core.windows.net/newcontainer/${images[1]}`} alt="" /></div>
                 </div>
@@ -134,11 +155,13 @@ const MainHebrew = () => {
                 </div>
                 <div class="hebrewLast">
                     <div style={{ backgroundImage: state }} className="hebrewScore" id="hebrewScore">
-                        0
+                        <span>Score</span>
+                        {score}
                     </div>
                     <div className="hebrewCard" onClick={() => flipCard(images[2])} ><img src={`https://absa7kzimnaf.blob.core.windows.net/newcontainer/${images[2]}`} alt="" /></div>
-                    <div style={{ backgroundImage: state }} className="mobileScore" id="hebrewScore">
-                        0
+                    <div style={{ backgroundImage: state }} className="mobileScore" id="mobileScore">
+                        <span>Score</span>
+                        {score}
                     </div>
                     <div className="hebrewCard" onClick={() => flipCard(images[3])} ><img src={`https://absa7kzimnaf.blob.core.windows.net/newcontainer/${images[3]}`} alt="" /></div>
                 </div>
@@ -149,6 +172,16 @@ const MainHebrew = () => {
             {failed && <div style={{ zIndex: "1000" }} className="congratHeb">
                 <FailedDiv nextWord={nextCard} />
             </div>}
+            <div style={{ position: "fixed" }} className="fab-container">
+                <div className="fab shadow">
+                    <div className="fab-content">
+                        <FontAwesomeIcon icon={faPlus} className="text-white" />
+                    </div>
+                </div>
+                <div className="sub-button shadow" onClick={() => navigate("/hebrew/leader")}>
+                    <FontAwesomeIcon icon={faUserGraduate} className="text-white" />
+                </div>
+            </div>
         </div>
     )
 }
